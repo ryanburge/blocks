2aee9b73be4c2d6537008d3a79d2f4549459a7fd
Ryan Burge <ryanburge@gmail.com>
2018-07-11 20:44:44 -0500

    I added a add_labels option to gg_col()

--- building/R/gg_col.R
+++ building/R/gg_col.R
@@ -1,4 +1,3 @@
-
 #' A Quick ggcol chart
 #'
 #' This function gives you a histogram with percentage annotations
@@ -6,13 +5,13 @@
 #' @param xvar What is going to be on the x-axis
 #' @param yvar What is going to be on the y-axis, usually pct
 #' @param fff What font you would like from Google fonts. Must be surrounded with quotation marks. Example: "Roboto". Defaults to Oswald.
+#' @param add_labels This will give you percentage above each bar. It's off by default. 
 #' @keywords plotting
 #' @export
 #' @examples
 #' gg_col()
 
-
-gg_col <- function (df, xvar, yvar, fff){
+gg_col <- function (df, xvar, yvar, fff, add_labels = FALSE){
   xvar <- enquo(xvar)
   yvar <- enquo(yvar)
   if(missing(fff)){
@@ -20,11 +19,10 @@ gg_col <- function (df, xvar, yvar, fff){
     
     showtext_auto()
     
-    ggplot(df, aes(x=!! xvar, y = !! yvar)) + 
+    a <- ggplot(df, aes(x=!! xvar, y = !! yvar)) + 
       geom_col(aes(fill = as.factor(!! xvar)), color = "black") +
       theme_minimal() + 
       theme(text=element_text(size=64, family="font")) +
-      geom_text(aes(y = !! yvar + .025, label = paste0(!! yvar*100, '%')), position = position_dodge(width = .9), size = 16, family = "font") +
       scale_y_continuous(labels = scales::percent) + theme(legend.position="none") 
     
   } else {
@@ -33,12 +31,19 @@ gg_col <- function (df, xvar, yvar, fff){
     
     showtext_auto()
     
-    ggplot(df, aes(x=!! xvar, y = !! yvar)) + 
+    a <- ggplot(df, aes(x=!! xvar, y = !! yvar)) + 
       geom_col(aes(fill = as.factor(!! xvar)), color = "black") +
       theme_minimal() + 
       theme(text=element_text(size=64, family="font")) +
-      geom_text(aes(y = !! yvar + .025, label = paste0(!! yvar*100, '%')), position = position_dodge(width = .9), size = 16, family = "font") +
       scale_y_continuous(labels = scales::percent) + theme(legend.position="none") 
+  }
+  
+  if(add_labels == TRUE){
+    
+    a + geom_text(aes(y = !! yvar + .025, label = paste0(!! yvar*100, '%')), position = position_dodge(width = .9), size = 16, family = "font")
+    
+  } else {
+    a
   }
   
 }
