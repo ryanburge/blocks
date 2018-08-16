4db9bea35f81484c71648582892ba6b76f4c8923
Ryan Burge <ryanburge@gmail.com>
2018-08-15 19:20:51 -0500

    added a weighting option to mean_ci()

--- building/R/mean_ci.R
+++ building/R/mean_ci.R
@@ -5,27 +5,29 @@
 #' @param df Name of the Dataset
 #' @param var Variable to find the mean of
 #' @param ci Confidence Interval, expressed as a decimal. i.e. .84. Defaults to .95
+#' @param wt Weight to be applied  
 #' @keywords Mean
 #' @export
 #' @examples
 #' mean_ci()
 
-mean_ci <- function(df, var, ci) {
+mean_ci <- function(df, var, wt = NULL, ci) {
   var <- enquo(var)
   ci <- enquo(ci)
+  wt <- enquo(wt)
   
   if(quo_is_missing(ci)) {
     df %>% 
-      summarise(mean = mean(!! var, na.rm = TRUE),
+      summarise(mean = weighted.mean(!! var, w = !! wt, na.rm = TRUE),
                 sd = sd(!! var, na.rm = TRUE), 
                 n = n()) %>% 
       mutate(se = sd/sqrt(n),
              lower = mean - qt(1 - (0.05 /2),  n -1) * se,
              upper = mean + qt(1 - (0.05 /2),  n -1) * se) 
   } else {
-  
-  df %>% 
-      summarise(mean = mean(!! var, na.rm = TRUE),
+    
+    df %>% 
+      summarise(mean = weightedmean(!! var, wt = !! wt, na.rm = TRUE),
                 sd = sd(!! var, na.rm = TRUE), 
                 n = n()) %>% 
       mutate(level = 1 - !! ci) %>% 
