b15660b0845fe7603d681b569cc92123ade32430
Ryan Burge <ryanburge@gmail.com>
2018-08-15 20:56:53 -0500

    fixed the mean_ci weight situation.

--- building/R/mean_ci.R
+++ building/R/mean_ci.R
@@ -11,31 +11,43 @@
 #' @examples
 #' mean_ci()
 
-mean_ci <- function(df, var, wt = NULL, ci) {
+mean_ci <- function(df, var, wt, ci = .95) {
   var <- enquo(var)
   ci <- enquo(ci)
   wt <- enquo(wt)
   
-  if(quo_is_missing(ci)) {
+  if(quo_is_missing(wt) & quo_is_missing(ci)) {
     df %>% 
-      summarise(mean = weighted.mean(!! var, w = !! wt, na.rm = TRUE),
+      summarise(mean = mean(!! var, na.rm = TRUE),
                 sd = sd(!! var, na.rm = TRUE), 
                 n = n()) %>% 
       mutate(se = sd/sqrt(n),
              lower = mean - qt(1 - (0.05 /2),  n -1) * se,
              upper = mean + qt(1 - (0.05 /2),  n -1) * se) 
-  } else {
-    
+  }
+  
+  if(quo_is_missing(wt)) {
     df %>% 
-      summarise(mean = weightedmean(!! var, wt = !! wt, na.rm = TRUE),
+      summarise(mean = mean(!! var,  na.rm = TRUE),
                 sd = sd(!! var, na.rm = TRUE), 
                 n = n()) %>% 
       mutate(level = 1 - !! ci) %>% 
       mutate(se = sd/sqrt(n),
              lower = mean - qt(1 - (level /2),  n -1) * se,
              upper = mean + qt(1 - (level /2),  n -1) * se) 
+  }
+  
+  else {
+    
+    df %>% 
+      summarise(mean = weighted.mean(!! var, w = !! wt, na.rm = TRUE),
+                sd = sd(!! var, na.rm = TRUE), 
+                n = n()) %>% 
+      mutate(level = 1 - !! ci) %>% 
+      mutate(se = sd/sqrt(n),
+             lower = mean - qt(1 - (level /2),  n -1) * se,
+             upper = mean + qt(1 - (level /2),  n -1) * se) %>% 
+      select(-level)
   } 
 }
-
-
 
